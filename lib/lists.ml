7193358e8b6e1e3581037500f261e40d5bc499fc
basokant <basokanthan@gmail.com>
2023-11-29 22:18:14 -0500

    add return types to functions

--- lib/lists.ml
+++ lib/lists.ml
@@ -1,25 +1,25 @@
 (** Return the last element of a list *)
-let rec last (list: 'a list) = 
+let rec last (list: 'a list): 'a option = 
   match list with
   | [] -> None
   | [ x ] -> Some x
   | _ :: t -> last t;;
 
 (** Return the last 2 elements of a list *)
-let rec last_two (list: 'a list) =
+let rec last_two (list: 'a list): ('a * 'a) option =
   match list with
   | [] | [ _ ] -> None
   | [ x; y ] -> Some (x, y)
   | _ :: t -> last_two t;;
 
 (** Return the nth element of a list *)
-let rec nth k (list: 'a list) =
+let rec nth k (list: 'a list): int option =
   match list with
   | [] -> None
   | h :: t -> if k = 0 then Some h else nth (k-1) t;;
 
 (** Return the length of a list *)
-let length (list: 'a list) =
+let length (list: 'a list): int =
   let rec aux n = function
     | [] -> n
     | _ :: t -> aux (n + 1) t
