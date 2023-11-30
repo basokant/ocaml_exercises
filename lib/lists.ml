f6147d7968de8042813879644a283071dd596c33
basokant <basokanthan@gmail.com>
2023-11-29 22:16:11 -0500

    add missing function documentation comments

--- lib/lists.ml
+++ lib/lists.ml
@@ -5,20 +5,25 @@ let rec last (list: 'a list) =
   | [ x ] -> Some x
   | _ :: t -> last t;;
 
+(** Return the last 2 elements of a list *)
 let rec last_two (list: 'a list) =
   match list with
   | [] | [ _ ] -> None
   | [ x; y ] -> Some (x, y)
   | _ :: t -> last_two t;;
 
+(** Return the nth element of a list *)
 let rec nth k (list: 'a list) =
   match list with
   | [] -> None
   | h :: t -> if k = 0 then Some h else nth (k-1) t;;
 
+(** Return the length of a list *)
 let length (list: 'a list) =
   let rec aux n = function
     | [] -> n
     | _ :: t -> aux (n + 1) t
   in
   aux 0 list;;
+
+
